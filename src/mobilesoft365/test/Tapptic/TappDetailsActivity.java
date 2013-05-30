1854673f9da08ad9573a734cdd2bf9a8a750ed8d
BruSD <BruSD.com@gmail.com>
2013-05-30 22:06:36 +0300

    Alpha Animation on Details View

--- src/mobilesoft365/test/Tapptic/TappDetailsActivity.java
+++ src/mobilesoft365/test/Tapptic/TappDetailsActivity.java
@@ -2,6 +2,11 @@ package mobilesoft365.test.Tapptic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,9 +16,84 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class TappDetailsActivity extends Activity {
+
+    ScrollView scrollAlpha;
+    Button OnOffAlfaButton;
+    AlphaAnimation offAnimation;
+    AlphaAnimation onAnimation;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tapp_details_layout);
 
+        OnOffAlfaButton = (Button)findViewById(R.id.on_off_button);
+
+
+
+        OnOffAlfaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowHideScroll();
+            }
+            });
+
     }
+
+    public void ShowHideScroll(){
+        scrollAlpha = (ScrollView)findViewById(R.id.scroll_alpha);
+        if (OnOffAlfaButton.getText() == getText(R.string.off_text))     {
+
+            offAnimation = new AlphaAnimation(1.0f, 0.0f);
+            offAnimation.setDuration(2000);
+            offAnimation.setFillAfter(true);
+            scrollAlpha.startAnimation(offAnimation);
+
+            offAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+                @Override
+                public void onAnimationEnd(Animation arg0) {
+                    OnOffAlfaButton.setEnabled(true);
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation arg0) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void onAnimationStart(Animation arg0) {
+                    // TODO Auto-generated method stub
+                    OnOffAlfaButton.setText(R.string.on_text);
+                    OnOffAlfaButton.setEnabled(false);
+                }
+
+            });
+        } else{
+
+            onAnimation = new AlphaAnimation(0.0f, 1.0f);
+            onAnimation.setDuration(2000);
+            onAnimation.setFillAfter(true);
+            scrollAlpha.startAnimation(onAnimation);
+            onAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    OnOffAlfaButton.setText(R.string.off_text);
+                    OnOffAlfaButton.setEnabled(false);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    OnOffAlfaButton.setEnabled(true);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }
+    }
+
+
 }
