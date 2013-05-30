6222b9449a5b074cd2fadeb9657159c53dfadbde
BruSD <BruSD.com@gmail.com>
2013-05-30 15:03:45 +0300

    1 commit

---
+++ src/mobilesoft365/test/Tapptic/MyActivity.java
@@ -0,0 +1,15 @@
+package mobilesoft365.test.Tapptic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
